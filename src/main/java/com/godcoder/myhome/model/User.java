de83bb611a04c473bf6b2a683116ce296342eeeb
Woonkil <woonkil70@gmail.com>
2021-12-20 02:26:45 +0900

    Initial commit

---
+++ src/main/java/com/godcoder/myhome/model/User.java
@@ -0,0 +1,29 @@
+package com.godcoder.myhome.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private Boolean enabled;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+    private List<Board> boards = new ArrayList<>();
+}
